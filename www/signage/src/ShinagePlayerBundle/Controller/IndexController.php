66bc64dd1696ab2da78dd25720e2ed97b01db68b
Michael Zapf <m.zapf@mztx.de>
2017-06-11 19:06:59 +0200

    improved frontend: check each 10s and load new presentation by url

--- www/signage/src/ShinagePlayerBundle/Controller/IndexController.php
+++ www/signage/src/ShinagePlayerBundle/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace mztx\ShinagePlayerBundle\Controller;
 
+use mztx\ShinagePlayerBundle\Service\GuidCreator;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -10,6 +11,10 @@ class IndexController extends Controller
 
     public function indexAction()
     {
+        /** @var GuidCreator $guidCreator */
+        #$guidCreator = $this->get('shinage.player.guid_creator');
+        #var_dump($guidCreator); exit;
+
         $rootDir = $this->get('kernel')->getRootDir() . '/../web/player.html';
         $appContent = file_get_contents($rootDir);
         return new Response($appContent);
